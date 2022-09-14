0e91bab5f477c711aeeb357d8a7e516e27cce797
Subhocr07 <96890352+Subhocr07@users.noreply.github.com>
2022-09-14 21:46:58 +0530

    DB connected

--- BlogWebSite/Server/index.js
+++ BlogWebSite/Server/index.js
@@ -4,11 +4,14 @@ const server = express();
 
 
 const PORT=process.env.PORT||7070;
-const MONGOURI="";
+const MONGOURI="mongodb+srv://InstaCluster:Subho2022@instaclauster.xpboj.mongodb.net/test";
 
 server.listen(PORT,(err)=>{
     !err? console.log(`Server started at ${PORT}`):Console.log(err);
 });
+mongoose.connect(MONGOURI,(err)=>{
+    !err? console.log(`DB connected at InstaCluster`): console.log(err);
+});
 server.get("/",(req,res)=>{
     res.send("Blog route started")
-})
+});
